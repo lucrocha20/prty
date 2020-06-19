f3bb11e0d100b63ab4990e08dc456a3024c40f7b
LUCAS DE MELLO ROCHA <lucas.rocha22@fatec.sp.gov.br>
2020-06-19 15:31:51 -0300

    Iniciando o projeto

---
+++ src/main/java/br/fatec/prty/PrtyApplication.java
@@ -0,0 +1,13 @@
+package br.fatec.prty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrtyApplication.class, args);
+	}
+
+}
