777d8ab71e3a04dd60a976644c2b84ef2fc3b6fe
Lucas Rocha <rocha.lucas20@gmail.com>
2020-06-29 18:21:05 -0300

    Alteracao CORS

--- src/main/java/br/fatec/prty/PrtyApplication.java
+++ src/main/java/br/fatec/prty/PrtyApplication.java
@@ -18,7 +18,8 @@ public class PrtyApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/").allowedOrigins("*");
+				registry.addMapping("/usuarios").allowedOrigins("*");
+				registry.addMapping("/eventos").allowedOrigins("*");
 			}
 		};
 	}
